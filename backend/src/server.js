10cc9743b532aaab57d7fc1e118c66fdb0337637
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2019-10-04 01:14:59 -0300

    Criando rota para sessão.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
 
+//Conexão com o banco do mongo db
+mongoose.connect(
+  "mongodb+srv://omnistack9:omnistack9@cluster0-0w4gj.mongodb.net/omnistack9?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
 app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
