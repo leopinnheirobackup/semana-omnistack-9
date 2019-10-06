dee6a341a53f047ef512a1d2622ceb89dc8b4f3c
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2019-10-06 14:49:00 -0300

    Adição do axios, configuração do axios e implementação de estado de e-mail.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "./services/api";
 import "./App.css";
 
 import logo from "./assets/logo.svg";
 
 function App() {
+  const [email, setEmail] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(email);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="Logo do AirCnC" />
@@ -12,11 +21,16 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre{" "}
           <strong>talentos para sua empresa</strong>
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
-          <input type="email" id="email" placeholder="Seu melhor e-mail" />
+          <input
+            type="email"
+            id="email"
+            placeholder="Seu melhor e-mail"
+            onChange={event => setEmail(event.target.value)}
+          />
 
-          <button class="btn" type="submit">
+          <button className="btn" type="submit">
             Entrar
           </button>
         </form>
