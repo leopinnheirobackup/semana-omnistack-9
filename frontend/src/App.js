ea198fd89264a1676d8fdb39a37a225bcf5635f7
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2019-10-06 15:12:35 -0300

    Consumindo api do backend, melhorando estrutura do projeto, com rotas.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,16 @@
-import React, { useState } from "react";
-import api from "./services/api";
+import React from "react";
 import "./App.css";
 
 import logo from "./assets/logo.svg";
 
+import Routes from "./routes";
+
 function App() {
-  const [email, setEmail] = useState("");
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const response = await api.post("/sessions", { email });
-
-    console.log(response.data);
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="Logo do AirCnC" />
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre{" "}
-          <strong>talentos para sua empresa</strong>
-        </p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Seu melhor e-mail"
-            onChange={event => setEmail(event.target.value)}
-          />
-
-          <button className="btn" type="submit">
-            Entrar
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
